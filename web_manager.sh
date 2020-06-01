b10d37556224c1bc8b5ee20d6546402b74426eee
SkeletonHoney <ryanerogers5@gmail.com>
2020-05-31 21:47:40 -0400

    web_manager now no longer outputs to CLI

--- web_manager.sh
+++ web_manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-	ansible-playbook playbooks/init_web.yml
-	ansible-playbook playbooks/splunk_forwarder_install_web.yml
+	ansible-playbook playbooks/init_web.yml 1>/dev/null 2>/dev/null
+	ansible-playbook playbooks/splunk_forwarder_install_web.yml 1>/dev/null 2>/dev/null
 	sleep 60
 done
