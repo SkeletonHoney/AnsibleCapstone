54102042e4768d6925c73b7c576b5dc948215d1c
SkeletonHoney <ryanerogers5@gmail.com>
2020-05-31 21:46:25 -0400

    fixed update_blacklist. dos_listener now runs continuously

--- dos_listener.sh
+++ dos_listener.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-#while true
-#do
-# Search for DOS attack
-SEARCH=$(python search.py "search HTTP OR SYN earliest_time=-50m | stats count by clientip" \
-	| grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' \
-	| sed 's/^/Require not ip /')
-if [ ! -z "$SEARCH" ]
-then
-	echo "Search found attacker"
-	# Check if result is already contained in blacklist
-	QUERY=$(grep -- "$SEARCH" ./playbooks/config/web/blacklist.conf)
-	if [ -z "$QUERY" ]
+while true
+do
+	# Search for DOS attack
+	SEARCH=$(python search.py "search HTTP OR SYN NOT 403 earliest_time=-15s | stats count by clientip | where count > 50" \
+		| grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' \
+		| sed 's/^/Require not ip /')
+	if [ ! -z "$SEARCH" ]
 	then
-		echo "Adding to blacklist"
-		# Add new rule to blacklist.conf and publish
-		echo "$SEARCH" >> ./playbooks/config/web/blacklist.conf
-		ansible-playbook ./playbooks/update_blacklist.yml
+		# Check if result is already contained in blacklist
+		QUERY=$(grep -- "$SEARCH" ./playbooks/config/web/blacklist.conf)
+		if [ -z "$QUERY" ]
+		then
+			# Add new rule to blacklist.conf and publish
+			echo "$SEARCH" >> ./playbooks/config/web/blacklist.conf
+			ansible-playbook ./playbooks/update_blacklist.yml 1> /dev/null 2> /dev/null
+		fi
 	fi
-fi
-#done
+	sleep 5 
+done
